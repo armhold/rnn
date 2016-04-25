7120ca2333e90f2182a61fc3f68357a6fee84ea4
George Armhold <armhold@gmail.com>
2016-04-25 19:56:28 -0400

    maybe working?

--- network.go
+++ network.go
@@ -1,12 +1,15 @@
 package piston
 
 import (
-	"github.com/gonum/matrix/mat64"
 	"math/rand"
 	"time"
 	"log"
 	"math"
+	"github.com/gonum/matrix/mat64"
 )
+
+import 	discreterand "github.com/dgryski/go-discreterand"
+
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
@@ -57,31 +60,78 @@ func (n *Network) Run(input string) {
 
 	iter := 0
 	p := 0
+	mWxh, mWhh, mWhy := zerosLike(n.Wxh), zerosLike(n.Whh), zerosLike(n.Why)
+	mbh, mby := zerosLike(n.bh), zerosLike(n.by) // memory variables for Adagrad
+	smooth_loss := -math.Log(1.0 / float64(n.VocabSize)) * SequenceLength // loss at iteration 0
 
 	var hprev *mat64.Dense
-	//var hprev []float64
 
 	inputs := make([]int, SequenceLength)
 	targets := make([]int, SequenceLength)
 
 	for {
+		// prepare inputs (we're sweeping from left to right in steps seq_length long)
 		if p + SequenceLength + 1 >= inputLen || iter == 0 {
-			// TODO: hprev = np.zeros()... etc
-			p = 0
-
-			//hprev = make([]float64, HiddenSize) // reset RNN memory
 			hprev = mat64.NewDense(HiddenSize, 1, nil) // reset RNN memory
+			p = 0  // go from start of data
 		}
-
 
 		for i := 0; i < SequenceLength; i++ {
 			inputs[i] = n.charToIndex[runes[p + i]]
 			targets[i] = n.charToIndex[runes[p + i + 1]]
 		}
+		//log.Printf("inputs: %q, p: %d", inputs, p)
 
-		log.Printf("inputs: %q, p: %d", inputs, p)
-		n.LossFunc(inputs, targets, hprev)
 
+		// sample from the model now and then
+		if iter % 100 == 0 {
+			sample_ix := n.sample(hprev, inputs[0], 200)
+			s := ""
+			chars := make([]rune, len(sample_ix))
+			for i, ix := range sample_ix {
+				ch := n.indexToChar[ix]
+				chars[i] = ch
+	 		}
+			s = string(chars)
+			log.Printf(s)
+		}
+
+		// forward seq_length characters through the net and fetch gradient
+		var loss float64
+		var dWxh, dWhh, dWhy, dbh, dby *mat64.Dense
+
+		loss, dWxh, dWhh, dWhy, dbh, dby, hprev = n.LossFunc(inputs, targets, hprev)
+		smooth_loss = smooth_loss * 0.999 + loss * 0.001
+		if iter % 100 == 0 {
+			log.Printf("iter %d, loss: %f", iter, smooth_loss) // print progress
+		}
+
+		// perform parameter update with Adagrad
+		//
+		doIt := func(param, dparam, mem *mat64.Dense) {
+
+			dSquared := &mat64.Dense{}
+			dSquared.MulElem(dparam, dparam)
+			mem.Add(mem, dSquared)
+
+			tmp := &mat64.Dense{}
+			tmp.Scale(-LearningRate, dparam)
+
+			sqrtMem := &mat64.Dense{}
+			sqrtMem.Apply(func(i, j int, v float64) float64 {
+				return math.Sqrt(v + 1e-8)   // adagrad update
+			}, mem)
+
+			tmp.DivElem(tmp, sqrtMem)
+			param.Add(param, tmp)
+
+		}
+
+		doIt(n.Wxh, dWxh, mWxh)
+		doIt(n.Whh, dWhh, mWhh)
+		doIt(n.Why, dWhy, mWhy)
+		doIt(n.bh, dbh, mbh)
+		doIt(n.by, dby, mby)
 
 		p = p + SequenceLength
 		iter += 1
@@ -90,19 +140,13 @@ func (n *Network) Run(input string) {
 
 
 
-func (n *Network) LossFunc(inputs, targets []int, hprev *mat64.Dense) {
-	charCount := len(inputs)
-
-	log.Printf("charCount: %d", charCount)
-
+func (n *Network) LossFunc(inputs, targets []int, hprev *mat64.Dense) (loss float64, dWxh *mat64.Dense, dWhh *mat64.Dense, dWhy *mat64.Dense, dbh *mat64.Dense, dby *mat64.Dense, lastHs *mat64.Dense ){
 	xs := make(map[int]*mat64.Dense)
 	hs := make(map[int]*mat64.Dense)
 	ys := make(map[int]*mat64.Dense)
 	ps := make(map[int]*mat64.Dense)
 
 	hs[-1] = mat64.DenseCopyOf(hprev)
-
-	var loss float64
 
 	// forward pass
 	//
@@ -112,27 +156,15 @@ func (n *Network) LossFunc(inputs, targets []int, hprev *mat64.Dense) {
 		xs[t] = mat64.NewDense(n.VocabSize, 1, nil)
 		xs[t].Set(inputs[t], 0, 1)
 
-		log.Printf("xs: %+v", xs)
-		log.Printf("hs: %+v", hs)
-		log.Printf("ys: %+v", ys)
-		log.Printf("ps: %+v", ps)
-		log.Printf("loss: %+v", loss)
-		log.Printf("t: %d", t)
-		log.Printf("n.Wxh: %+v", n.Wxh)
-
-
 		// hidden state
 		//
 		dot1 := dot(n.Wxh, xs[t])
-		log.Printf("dot1: %+v", dot1)
 
 		dot2 := dot(n.Whh, hs[t-1])
-		log.Printf("dot2: %+v", dot2)
 
 		hs[t] = &mat64.Dense{}
 		hs[t].Add(dot1, dot2)
 		hs[t].Add(hs[t], n.bh)
-		log.Printf("hs[t]: %+v", hs[t])
 
 		hs[t].Apply(func(i, j int, v float64) float64 {
 			return math.Tanh(v)
@@ -144,33 +176,19 @@ func (n *Network) LossFunc(inputs, targets []int, hprev *mat64.Dense) {
 		ys[t].Mul(n.Why, hs[t])
 		ys[t].Add(ys[t], n.by)
 
-
 		// probabilities for next chars
 		//
-		exp := &mat64.Dense{}
-		exp.Apply(func(i, j int, v float64) float64 {
-			return math.Exp(v)
-		}, ys[t])
-
-		sum := mat64.Sum(exp)
-
-		ps[t] = &mat64.Dense{}
-		ps[t].Apply(func(i, j int, v float64) float64 {
-			return v / sum
-		}, exp)
-
+		ps[t] = expDivSumExp(ys[t])
 
 		// softmax (cross-entropy loss)
-		loss += math.Log(ps[t].At(targets[t], 0))
+		loss += - math.Log(ps[t].At(targets[t], 0))
 	}
 
 	//  backward pass: compute gradients going backwards
 	//
-	dWxh, dWhh, dWhy := zerosLike(n.Wxh), zerosLike(n.Whh), zerosLike(n.Why)
-	dbh, dby := zerosLike(n.bh), zerosLike(n.by)
+	dWxh, dWhh, dWhy = zerosLike(n.Wxh), zerosLike(n.Whh), zerosLike(n.Why)
+	dbh, dby = zerosLike(n.bh), zerosLike(n.by)
 	dhnext := zerosLike(hs[0])
-
-	log.Print(dWxh, dWhh, dWhy, dbh, dby, dhnext)
 
 	for t := len(inputs) - 1; t >= 0; t-- {
 		dy := mat64.DenseCopyOf(ps[t])
@@ -199,10 +217,45 @@ func (n *Network) LossFunc(inputs, targets []int, hprev *mat64.Dense) {
 
 	// clip to mitigate exploding gradients
 	clipTo(-5, 5, dWxh, dWhh, dWhy, dbh, dby)
+
+	lastHs = hs[len(inputs) - 1]
+	return
 }
 
+func (n *Network) sample(h *mat64.Dense, seedIx, count  int) []int {
+	x := mat64.NewDense(n.VocabSize, 1, nil)
+	x.Set(seedIx, 0, 1)
+
+	var ixes []int
+
+	for i := 0; i < count; i++ {
+		dot1 := dot(n.Wxh, x)
+		dot2 := dot(n.Whh, h)
+
+		h.Add(dot1, dot2)
+		h.Add(h, n.bh)
+
+		h.Apply(func(i, j int, v float64) float64 {
+			return math.Tanh(v)
+		}, h)
 
 
+		y := dot(n.Why, h)
+		y.Add(y, n.by)
+
+		p := expDivSumExp(y)
+		at := discreterand.NewAlias(ravel(p), rand.NewSource(time.Now().UTC().UnixNano()))
+		index := at.Next()
+		r := rangeToArray(n.VocabSize)
+		ix := r[index]
+		log.Printf("chose index %d -> %d", index, ix)
+		x := mat64.NewDense(n.VocabSize, 1, nil)
+		x.Set(ix, 0, 1)
+		ixes = append(ixes, ix)
+	}
+
+	return ixes
+}
 
 func randomMatrix(rows, cols int) *mat64.Dense {
 	result := mat64.NewDense(rows, cols, nil)
@@ -210,8 +263,6 @@ func randomMatrix(rows, cols int) *mat64.Dense {
 
 	return result
 }
-
-
 
 func randomize(m *mat64.Dense) {
 	r, c := m.Dims()
@@ -265,4 +316,49 @@ func clipTo(leftRange, rightRange float64, matrices ...*mat64.Dense) {
 			return v
 		}, m)
 	}
+}
+
+func expDivSumExp(m *mat64.Dense) *mat64.Dense {
+	exp := &mat64.Dense{}
+	exp.Apply(func(i, j int, v float64) float64 {
+		return math.Exp(v)
+	}, m)
+
+	sumExp := mat64.Sum(exp)
+
+	result := &mat64.Dense{}
+	result.Apply(func(i, j int, v float64) float64 {
+		return v / sumExp
+	}, exp)
+
+	return result
+}
+
+// TODO: unit test
+func ravel(m *mat64.Dense) []float64 {
+	r, c := m.Dims()
+
+	result := make([]float64, r * c)
+
+	i := 0
+
+	for row := 0; row < r; row++ {
+		for col := 0; col < c; col++ {
+			result[i] = m.At(row, col)
+			i++
+		}
+	}
+
+	return result
+}
+
+// emulate Python's range()
+func rangeToArray(n int) []int {
+	result := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		result[i] = i
+	}
+
+	return result
 }
